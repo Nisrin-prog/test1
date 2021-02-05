0d56f65e3fe7ac24ca1265912dec5751f8e2418b
Nisrin-prog <wisenish@gmail.com>
2021-02-05 21:40:36 +0530

    added a bird

--- sketch.js
+++ sketch.js
@@ -35,7 +35,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    bird2 = new Bird(200,200)
 }
 
 function draw(){
@@ -60,5 +60,6 @@ function draw(){
     log5.display();
 
     bird.display();
+    bird2.display();
     platform.display();
 }
